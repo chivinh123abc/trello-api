04966c2055c932899e4396764c395648eec72d59
chivinh123abc <mail.lcvinh@gmail.com>
2025-04-04 22:23:27 +0700

    Error handling

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import exitHook from 'async-exit-hook'
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
 import { APIs_V1 } from '~/routes/v1'
+import { errorHandlingMiddleware } from './middlewares/errorHandlingMiddleware'
 
 
 const START_SERVER = () => {
@@ -13,6 +14,8 @@ const START_SERVER = () => {
   app.use(express.json())
   //Use APIs v1
   app.use('/v1', APIs_V1)
+  //Middleware xu ly loi tap trung
+  app.use(errorHandlingMiddleware)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`3. Hello ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}/v1/`)
