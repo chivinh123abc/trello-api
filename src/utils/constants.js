21e735170d292774c8822294bf21f077399321cd
chivinh123abc <mail.lcvinh@gmail.com>
2025-04-12 20:00:53 +0700

    Update CORS now Aloow Local  host

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 //Nhung domain dc truy cap den tai nguyen cua server
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173'
+  // 'http://localhost:5173'
   //vi du sau nay  se deploy len  domain chinh thuc
 ]
 
